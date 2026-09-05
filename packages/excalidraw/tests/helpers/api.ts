dwelle/excalidraw

feat: element animation system (fade & fly) with stagger and easing
Port of barnabasmolnar/animations-v2 onto latest master, squashed into a
single commit. Adds an imperative element animation API to the editor:

- animateElements() on the imperative API: fade/fly animations with
  per-element duration, delay, stagger, and easing (linear, easeOut,
  easeInOut); returns a batch handle with finished promise plus
  finish()/cancel()/getStatus() for completion signaling
- cancelElementAnimation() and clearElementAnimationOverrides() APIs
- resolveRenderOpacity prop for host-controlled render opacity
- render caches are preserved during animations: position/opacity
  overrides are applied at draw time (positionOffset / context
  translation) instead of mutating elements, keyed by a render
  animation version
- bound text elements animate together with their containers
- example app (with-script-in-browser) animation playground and
  initialData demo

Co-Authored-By: Claude Fable 5 <noreply@anthropic.com>

packages/excalidraw/tests/helpers/api.ts
@@ -24,7 +24,10 @@ import {
   newTextElement,
 } from "@excalidraw/element";
 
-import { isUsingAdaptiveRadius, getSelectedElements } from "@excalidraw/element";
+import {
+  isUsingAdaptiveRadius,
+  getSelectedElements,
+} from "@excalidraw/element";
 import { selectGroupsForSelectedElements } from "@excalidraw/element";
 
 import { FONT_SIZES } from "@excalidraw/common";
@@ -46,6 +49,7 @@ import type {
 } from "@excalidraw/element/types";
 
 import type { Mutable } from "@excalidraw/common/utility-types";
+import type { ElementUpdate } from "@excalidraw/element";
 
 import { getMimeType } from "../../data/blob";
 import { createTestHook } from "../../components/App";
@@ -55,7 +59,6 @@ import { GlobalTestState, createEvent, fireEvent, act } from "../test-utils";
 import type { Action } from "../../actions/types";
 import type App from "../../components/App";
 import type { AppState } from "../../types";
-
 
 const readFile = util.promisify(fs.readFile);
 // so that window.h is available when App.tsx is not imported as well.
@@ -84,31 +87,35 @@ export class API {
     });
   };
 
-  static setSelectedElements = (elements: ExcalidrawElement[], editingGroupId?: string | null) => {
+  static setSelectedElements = (
+    elements: ExcalidrawElement[],
+    editingGroupId?: string | null,
+  ) => {
     act(() => {
       h.setState({
         ...selectGroupsForSelectedElements(
-        {
-          editingGroupId: editingGroupId ?? null,
-          selectedElementIds: elements.reduce((acc, element) => {
-            acc[element.id] = true;
-            return acc;
-          }, {} as Record<ExcalidrawElement["id"], true>),
-        },
-        elements,
-        h.state,
-        h.app,
-        )
+          {
+            editingGroupId: editingGroupId ?? null,
+            selectedElementIds: elements.reduce((acc, element) => {
+              acc[element.id] = true;
+              return acc;
+            }, {} as Record<ExcalidrawElement["id"], true>),
+          },
+          elements,
+          h.state,
+          h.app,
+        ),
       });
     });
   };
 
-  // eslint-disable-next-line prettier/prettier
   static updateElement = <T extends ExcalidrawElement>(
-    ...args: Parameters<typeof h.app.scene.mutateElement<T>>
+    element: T,
+    updates: ElementUpdate<T>,
+    options?: Parameters<App["scene"]["mutateElement"]>[2],
   ) => {
     act(() => {
-      h.app.scene.mutateElement(...args);
+      h.app.scene.mutateElement(element as Mutable<T>, updates, options);
     });
   };
 
@@ -155,8 +162,11 @@ export class API {
   };
 
   static getElement = <T extends ExcalidrawElement>(element: T): T => {
-    return h.app.scene.getElementsMapIncludingDeleted().get(element.id) as T || element;
-  }
+    return (
+      (h.app.scene.getElementsMapIncludingDeleted().get(element.id) as T) ||
+      element
+    );
+  };
 
   static createElement = <
     T extends Exclude<ExcalidrawElementType, "selection"> = "rectangle",
@@ -204,7 +214,9 @@ export class API {
     containerId?: T extends "text"
       ? ExcalidrawTextElement["containerId"]
       : never;
-    points?: T extends "arrow" | "line" | "freedraw" ? readonly LocalPoint[] : never;
+    points?: T extends "arrow" | "line" | "freedraw"
+      ? readonly LocalPoint[]
+      : never;
     strokeOptions?: T extends "freedraw"
       ? ExcalidrawFreeDrawElement["strokeOptions"]
       : never;
@@ -213,16 +225,24 @@ export class API {
     scale?: T extends "image" ? ExcalidrawImageElement["scale"] : never;
     status?: T extends "image" ? ExcalidrawImageElement["status"] : never;
     startBinding?: T extends "arrow"
-      ? ExcalidrawArrowElement["startBinding"] | ExcalidrawElbowArrowElement["startBinding"]
+      ?
+          | ExcalidrawArrowElement["startBinding"]
+          | ExcalidrawElbowArrowElement["startBinding"]
       : never;
     endBinding?: T extends "arrow"
-      ? ExcalidrawArrowElement["endBinding"] | ExcalidrawElbowArrowElement["endBinding"]
+      ?
+          | ExcalidrawArrowElement["endBinding"]
+          | ExcalidrawElbowArrowElement["endBinding"]
       : never;
     startArrowhead?: T extends "arrow"
-      ? ExcalidrawArrowElement["startArrowhead"] | ExcalidrawElbowArrowElement["startArrowhead"]
+      ?
+          | ExcalidrawArrowElement["startArrowhead"]
+          | ExcalidrawElbowArrowElement["startArrowhead"]
       : never;
     endArrowhead?: T extends "arrow"
-      ? ExcalidrawArrowElement["endArrowhead"] | ExcalidrawElbowArrowElement["endArrowhead"]
+      ?
+          | ExcalidrawArrowElement["endArrowhead"]
+          | ExcalidrawElbowArrowElement["endArrowhead"]
       : never;
     elbowed?: boolean;
     fixedSegments?: FixedSegment[] | null;
@@ -277,8 +297,8 @@ export class API {
       )
         ? {
             type: isUsingAdaptiveRadius(type)
-                    ? ROUNDNESS.ADAPTIVE_RADIUS
-                    : ROUNDNESS.PROPORTIONAL_RADIUS,
+              ? ROUNDNESS.ADAPTIVE_RADIUS
+              : ROUNDNESS.PROPORTIONAL_RADIUS,
           }
         : null,
       roughness: rest.roughness ?? appState.currentItemRoughness,
@@ -424,18 +444,15 @@ export class API {
         opts?.label?.frameId === undefined
           ? opts?.frameId ?? null
           : opts?.label?.frameId ?? null,
-      groupIds: opts?.label?.groupIds === undefined
-      ? opts?.groupIds
-      : opts?.label?.groupIds ,
-
+      groupIds:
+        opts?.label?.groupIds === undefined
+          ? opts?.groupIds
+          : opts?.label?.groupIds,
     });
 
-    h.app.scene.mutateElement(
-      rectangle,
-      {
-        boundElements: [{ type: "text", id: text.id }],
-      },
-    );
+    h.app.scene.mutateElement(rectangle, {
+      boundElements: [{ type: "text", id: text.id }],
+    });
 
     return [rectangle, text];
   };
@@ -463,12 +480,9 @@ export class API {
           : opts?.label?.frameId ?? null,
     });
 
-    h.app.scene.mutateElement(
-      arrow,
-      {
-        boundElements: [{ type: "text", id: text.id }],
-      },
-    );
+    h.app.scene.mutateElement(arrow, {
+      boundElements: [{ type: "text", id: text.id }],
+    });
 
     return [arrow, text];
   };
@@ -490,13 +504,23 @@ export class API {
     });
   };
 
-  static drop = async (items: ({kind: "string", value: string, type: string} | {kind: "file", file: File | Blob, type?: string })[]) => {
-
+  static drop = async (
+    items: (
+      | { kind: "string"; value: string; type: string }
+      | { kind: "file"; file: File | Blob; type?: string }
+    )[],
+  ) => {
     const fileDropEvent = createEvent.drop(GlobalTestState.interactiveCanvas);
 
-    const dataTransferFileItems = items.filter(i => i.kind === "file") as {kind: "file", file: File | Blob, type: string }[];
+    const dataTransferFileItems = items.filter((i) => i.kind === "file") as {
+      kind: "file";
+      file: File | Blob;
+      type: string;
+    }[];
 
-    const files = dataTransferFileItems.map(item => item.file) as File[] & { item: (index: number) => File };
+    const files = dataTransferFileItems.map((item) => item.file) as File[] & {
+      item: (index: number) => File;
+    };
     // https://developer.mozilla.org/en-US/docs/Web/API/FileList/item
     files.item = (index: number) => files[index];
 
@@ -506,7 +530,7 @@ export class API {
         files,
         // https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/items
         items: items.map((item, idx) => {
-          if (item.kind === "string")  {
+          if (item.kind === "string") {
             return {
               kind: "string",
               type: item.type,
@@ -523,10 +547,18 @@ export class API {
         }),
         // https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/getData
         getData: (type: string) => {
-          return items.find((item) => item.type === "string" && item.type === type) || "";
+          return (
+            items.find(
+              (item) => item.type === "string" && item.type === type,
+            ) || ""
+          );
         },
         // https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/types
-        types: Array.from(new Set(items.map((item) => item.kind === "file" ? "Files" : item.type))),
+        types: Array.from(
+          new Set(
+            items.map((item) => (item.kind === "file" ? "Files" : item.type)),
+          ),
+        ),
       },
     });
     Object.defineProperty(fileDropEvent, "clientX", {

barnabasmolnar/dwelle-excalidraw

packages/excalidraw/tests/helpers/api.ts
@@ -24,7 +24,10 @@ import {
   newTextElement,
 } from "@excalidraw/element";
 
-import { isUsingAdaptiveRadius, getSelectedElements } from "@excalidraw/element";
+import {
+  isUsingAdaptiveRadius,
+  getSelectedElements,
+} from "@excalidraw/element";
 import { selectGroupsForSelectedElements } from "@excalidraw/element";
 
 import { FONT_SIZES } from "@excalidraw/common";
@@ -48,6 +51,7 @@ import type {
 } from "@excalidraw/element/types";
 
 import type { Mutable } from "@excalidraw/common/utility-types";
+import type { ElementUpdate } from "@excalidraw/element";
 
 import { getMimeType } from "../../data/blob";
 import { createTestHook } from "../../components/App";
@@ -57,7 +61,6 @@ import { GlobalTestState, createEvent, fireEvent, act } from "../test-utils";
 import type { Action } from "../../actions/types";
 import type App from "../../components/App";
 import type { AppState } from "../../types";
-
 
 const readFile = util.promisify(fs.readFile);
 // so that window.h is available when App.tsx is not imported as well.
@@ -86,31 +89,35 @@ export class API {
     });
   };
 
-  static setSelectedElements = (elements: NonDeletedExcalidrawElement[], editingGroupId?: string | null) => {
+  static setSelectedElements = (
+    elements: NonDeletedExcalidrawElement[],
+    editingGroupId?: string | null,
+  ) => {
     act(() => {
       h.setState({
         ...selectGroupsForSelectedElements(
-        {
-          editingGroupId: editingGroupId ?? null,
-          selectedElementIds: elements.reduce((acc, element) => {
-            acc[element.id] = true;
-            return acc;
-          }, {} as Record<ExcalidrawElement["id"], true>),
-        },
-        elements,
-        h.state,
-        h.app,
-        )
+          {
+            editingGroupId: editingGroupId ?? null,
+            selectedElementIds: elements.reduce((acc, element) => {
+              acc[element.id] = true;
+              return acc;
+            }, {} as Record<ExcalidrawElement["id"], true>),
+          },
+          elements,
+          h.state,
+          h.app,
+        ),
       });
     });
   };
 
-  // eslint-disable-next-line prettier/prettier
   static updateElement = <T extends ExcalidrawElement>(
-    ...args: Parameters<typeof h.app.scene.mutateElement<T>>
+    element: T,
+    updates: ElementUpdate<T>,
+    options?: Parameters<App["scene"]["mutateElement"]>[2],
   ) => {
     act(() => {
-      h.app.scene.mutateElement(...args);
+      h.app.scene.mutateElement(element as Mutable<T>, updates, options);
     });
   };
 
@@ -157,8 +164,11 @@ export class API {
   };
 
   static getElement = <T extends ExcalidrawElement>(element: T): T => {
-    return h.app.scene.getElementsMapIncludingDeleted().get(element.id) as T || element;
-  }
+    return (
+      (h.app.scene.getElementsMapIncludingDeleted().get(element.id) as T) ||
+      element
+    );
+  };
 
   static createElement = <
     T extends Exclude<ExcalidrawElementType, "selection"> = "rectangle",
@@ -216,16 +226,24 @@ export class API {
     scale?: T extends "image" ? ExcalidrawImageElement["scale"] : never;
     status?: T extends "image" ? ExcalidrawImageElement["status"] : never;
     startBinding?: T extends "arrow"
-      ? ExcalidrawArrowElement["startBinding"] | ExcalidrawElbowArrowElement["startBinding"]
+      ?
+          | ExcalidrawArrowElement["startBinding"]
+          | ExcalidrawElbowArrowElement["startBinding"]
       : never;
     endBinding?: T extends "arrow"
-      ? ExcalidrawArrowElement["endBinding"] | ExcalidrawElbowArrowElement["endBinding"]
+      ?
+          | ExcalidrawArrowElement["endBinding"]
+          | ExcalidrawElbowArrowElement["endBinding"]
       : never;
     startArrowhead?: T extends "arrow"
-      ? ExcalidrawArrowElement["startArrowhead"] | ExcalidrawElbowArrowElement["startArrowhead"]
+      ?
+          | ExcalidrawArrowElement["startArrowhead"]
+          | ExcalidrawElbowArrowElement["startArrowhead"]
       : never;
     endArrowhead?: T extends "arrow"
-      ? ExcalidrawArrowElement["endArrowhead"] | ExcalidrawElbowArrowElement["endArrowhead"]
+      ?
+          | ExcalidrawArrowElement["endArrowhead"]
+          | ExcalidrawElbowArrowElement["endArrowhead"]
       : never;
     elbowed?: boolean;
     fixedSegments?: FixedSegment[] | null;
@@ -282,8 +300,8 @@ export class API {
       )
         ? {
             type: isUsingAdaptiveRadius(type)
-                    ? ROUNDNESS.ADAPTIVE_RADIUS
-                    : ROUNDNESS.PROPORTIONAL_RADIUS,
+              ? ROUNDNESS.ADAPTIVE_RADIUS
+              : ROUNDNESS.PROPORTIONAL_RADIUS,
           }
         : null,
       roughness: rest.roughness ?? appState.currentItemRoughness,
@@ -430,18 +448,15 @@ export class API {
         opts?.label?.frameId === undefined
           ? opts?.frameId ?? null
           : opts?.label?.frameId ?? null,
-      groupIds: opts?.label?.groupIds === undefined
-      ? opts?.groupIds
-      : opts?.label?.groupIds ,
-
+      groupIds:
+        opts?.label?.groupIds === undefined
+          ? opts?.groupIds
+          : opts?.label?.groupIds,
     });
 
-    h.app.scene.mutateElement(
-      rectangle,
-      {
-        boundElements: [{ type: "text", id: text.id }],
-      },
-    );
+    h.app.scene.mutateElement(rectangle, {
+      boundElements: [{ type: "text", id: text.id }],
+    });
 
     return [rectangle, text];
   };
@@ -469,12 +484,9 @@ export class API {
           : opts?.label?.frameId ?? null,
     });
 
-    h.app.scene.mutateElement(
-      arrow,
-      {
-        boundElements: [{ type: "text", id: text.id }],
-      },
-    );
+    h.app.scene.mutateElement(arrow, {
+      boundElements: [{ type: "text", id: text.id }],
+    });
 
     return [arrow, text];
   };
@@ -496,13 +508,23 @@ export class API {
     });
   };
 
-  static drop = async (items: ({kind: "string", value: string, type: string} | {kind: "file", file: File | Blob, type?: string })[]) => {
-
+  static drop = async (
+    items: (
+      | { kind: "string"; value: string; type: string }
+      | { kind: "file"; file: File | Blob; type?: string }
+    )[],
+  ) => {
     const fileDropEvent = createEvent.drop(GlobalTestState.interactiveCanvas);
 
-    const dataTransferFileItems = items.filter(i => i.kind === "file") as {kind: "file", file: File | Blob, type: string }[];
+    const dataTransferFileItems = items.filter((i) => i.kind === "file") as {
+      kind: "file";
+      file: File | Blob;
+      type: string;
+    }[];
 
-    const files = dataTransferFileItems.map(item => item.file) as File[] & { item: (index: number) => File };
+    const files = dataTransferFileItems.map((item) => item.file) as File[] & {
+      item: (index: number) => File;
+    };
     // https://developer.mozilla.org/en-US/docs/Web/API/FileList/item
     files.item = (index: number) => files[index];
 
@@ -512,7 +534,7 @@ export class API {
         files,
         // https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/items
         items: items.map((item, idx) => {
-          if (item.kind === "string")  {
+          if (item.kind === "string") {
             return {
               kind: "string",
               type: item.type,
@@ -529,10 +551,18 @@ export class API {
         }),
         // https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/getData
         getData: (type: string) => {
-          return items.find((item) => item.type === "string" && item.type === type) || "";
+          return (
+            items.find(
+              (item) => item.type === "string" && item.type === type,
+            ) || ""
+          );
         },
         // https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/types
-        types: Array.from(new Set(items.map((item) => item.kind === "file" ? "Files" : item.type))),
+        types: Array.from(
+          new Set(
+            items.map((item) => (item.kind === "file" ? "Files" : item.type)),
+          ),
+        ),
       },
     });
     Object.defineProperty(fileDropEvent, "clientX", {